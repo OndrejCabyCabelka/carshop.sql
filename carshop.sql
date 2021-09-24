91bc5c7717bd4e07f18d5329e76d2516b3ab9f1e
OndrejCabyCabelka <79140324+OndrejCabyCabelka@users.noreply.github.com>
2021-09-24 13:00:21 +0200

    carshop2

--- carshop.sql
+++ carshop.sql
@@ -1,7 +1,46 @@
 CREATE DATABASE carshop;
 
-CREATE TABLE car (
+CREATE TABLE cars (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY;
     manufacturer VARCHAR(255),
     model VARCHAR(255)
+
 );
+
+    DESCRIBE car;
+
+
+    --vlkádání řádnků do tabulky
+    INSERT INTO cars (id, model, manufacturer);
+    VALUES  (1, 'i30', 'Hyundai');
+
+
+    SELECT * 
+    FROM cars;
+
+
+    DROP TABLE carss;
+
+
+    INSERT INTO cars (model, manufacturer)     VALUES  ('Arteon', 'Volkswagen');
+ INSERT INTO cars (model, manufacturer)     VALUES  ('Passat Variant', 'Volkswage…
+Upravit 10:22:21 INSERT INTO cars (model, manufacturer)     VALUES  ('Passat', 'Volkswagen');
+Upravit 10:22:05 INSERT INTO cars (model, manufacturer)     VALUES  ('ID.4', 'Volkswagen');
+Upravit 10:21:53 INSERT INTO cars (model, manufacturer)     VALUES  ('Tiguan', 'Volkswagen');
+Upravit 10:21:44 INSERT INTO cars (model, manufacturer)     VALUES  ('Touran', 'Volkswagen');
+Upravit 10:21:32 INSERT INTO cars (model, manufacturer)     VALUES  ('Golf', 'Volkswagen');
+Upravit 10:21:15 INSERT INTO cars (model, manufacturer)     VALUES  ('ID.3', 'Volkswagen');
+Upravit 10:20:15 INSERT INTO cars (model, manufacturer)     VALUES  ('T-Roc"', 'Volkswagen');
+Upravit 10:20:04 INSERT INTO cars (model, manufacturer)     VALUES  ('T-cross"', 'Volkswagen');
+Upravit 10:19:51 INSERT INTO cars (model, manufacturer)     VALUES  ('Taigo"', 'Volkswagen');
+Upravit 10:19:40 INSERT INTO cars (model, manufacturer)     VALUES  ('Polo"', 'Volkswagen');
+Upravit 10:19:24 INSERT INTO cars (model, manufacturer)     VALUES  ('Up"', 'Volkswagen');
+Upravit 10:18:26 INSERT INTO cars (model, manufacturer)     VALUES  ('Superb', 'Škoda');
+Upravit 10:17:54 INSERT INTO cars (model, manufacturer)     VALUES  ('Kamiq', 'Škoda');
+Upravit 10:17:40 INSERT INTO cars (model, manufacturer)     VALUES  ('Scala', 'Škoda');
+Upravit 10:16:58 INSERT INTO cars (model, manufacturer)     VALUES  ('Fabia', 'Škoda');
+Upravit 10:16:44 INSERT INTO cars (model, manufacturer)     VALUES  ('Karoq', 'Škoda');
+Upravit 10:16:31 INSERT INTO cars (model, manufacturer)     VALUES  ('Kodiaq', 'Škoda');
+Upravit 10:16:22 INSERT INTO cars (model, manufacturer)     VALUES  ('Oktavia', 'Škoda');
+Upravit 10:15:53 INSERT INTO cars (model, manufacturer)     VALUES  ('i40', 'Hyundai');
+Upravit 10:15:34 INSERT INTO cars (model, manufacturer)     VALUES  ('i30', 'Hyundai');
